d5b30d9fc8b1df2e1657f132f7cd5e509586eef8
JooHeon-Lee <58197251+JooHeon-Lee@users.noreply.github.com>
2022-07-27 20:07:52 +0900

    0727 서버&클라 연동 및 한번에 run

--- my-blog/server/server.js
+++ my-blog/server/server.js
@@ -3,8 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // 서버 응답 출력
-app.get('/', (req, res) => {
-    res.send(`Response Complete`);
+app.get('/api/host', (req, res) => {
+    res.send({ host : 'JooHeon' });
 })
 
 // 서버 실행 ( server.js 가 있는 디렉토리에서 실행해야함.)
