4a63fd8126cddf4acadc15320840800e6acd75fe
JooHeon-Lee <58197251+JooHeon-Lee@users.noreply.github.com>
2022-07-27 19:26:53 +0900

    0727

---
+++ my-blog/server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// 서버 응답 출력
+app.get('/', (req, res) => {
+    res.send(`Response Complete`);
+})
+
+// 서버 실행 ( server.js 가 있는 디렉토리에서 실행해야함.)
+
+app.listen(PORT,() => {
+    console.log(`Server ON :: http://localhost:${PORT}/`);
+})
