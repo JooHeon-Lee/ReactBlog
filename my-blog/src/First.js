38c35a1ae0c7462be2ab01facb7474f9e87379ab
JooHeon-Lee <58197251+JooHeon-Lee@users.noreply.github.com>
2022-09-28 20:35:12 +0900

    0928

--- my-blog/src/First.js
+++ my-blog/src/First.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import { Link } from "react-router-dom";
 
 
+
 const First = (props) => {
+
+    const [count, setCount] = useState(0);
+    const countUp = () => setCount(count + 1);
+
+    useEffect(() => {
+        console.log("useEffect!!",count)
+    },[count])
+
     return(
         <>
             <h1>First Page</h1>
             <ul>
                 <Link to='/'><li>Main</li></Link>
             </ul>
+            <p>{count}번 클릭!</p>
+            <button onClick={countUp}>Click me</button>
         </>
     );
 };
