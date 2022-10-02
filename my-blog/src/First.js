7c74faed45bdff40cd370c557c5e540febb3387a
JooHeon-Lee <58197251+JooHeon-Lee@users.noreply.github.com>
2022-10-02 20:51:30 +0900

    1002

--- my-blog/src/First.js
+++ my-blog/src/First.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 
 const First = (props) => {
-
+    const [myName, setMyName] = useState("LJH")
     const [count, setCount] = useState(0);
     const countUp = () => setCount(count + 1);
 
@@ -19,7 +19,13 @@ const First = (props) => {
                 <Link to='/'><li>Main</li></Link>
             </ul>
             <p>{count}번 클릭!</p>
+            <p>안녕하세요 {myName}입니다.</p>
             <button onClick={countUp}>Click me</button>
+            <button 
+                onClick={() => {
+                    setMyName(myName === "LJH" ? "JooHeon" : "jh");
+                }}
+            >Change</button>
         </>
     );
 };
